a64854168e28091b6511084c00d4b7c352445947
RisingAce <harry9092@gmail.com>
2025-05-03 04:19:57 +0100

    feat: Add Google AdSense integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import "./globals.css"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
+import Script from "next/script"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,6 +20,14 @@ export default function RootLayout({
   // ThemeProvider should wrap the children INSIDE the body tag
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7602339756703410"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={inter.className}>
         <ThemeProvider 
           attribute="class" 
